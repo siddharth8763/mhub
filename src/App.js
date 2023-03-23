06c0393ad756abdd537dce5052aa4e5807e4709c
Siddharth <siddharth8763@gmail.com>
2023-03-23 20:46:40 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import axios from "axios";
+import { Routes, Route } from "react-router-dom";
+import Layout from "./Components/Layout";
+import Header from "./Components/Header/Header";
+import Home from "./Components/Home/Home"
+import Trailer from "./Components/Trailer/Trailer";
+import Reviews from "./Components/Reviews/Reviews";
+import NotFound from "./Components/NotFound/NotFound";
+import "./App.css";
 
 function App() {
+  const [movies, setMovies] = useState();
+  const [movie, setMovie] = useState();
+  const [reviews, setReviews] = useState([]);
+
+  const getMovies = async () => {
+    try {
+      const response = await axios.get("http://localhost:8080/api/v1/movies");
+      setMovies(response.data);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const getMovieData = async (movieId) => {
+    try {
+      const response = await axios.get(`http://localhost:8080/api/v1/movies/${movieId}`);
+
+      const singleMovie = response.data;
+
+      setMovie(singleMovie);
+      setReviews(singleMovie.reviews);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    getMovies();
+  }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header/>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route path="/" element={<Home movies={movies} />} ></Route>
+            <Route path="/Trailer/:ytTrailerId" element={<Trailer/>}></Route>
+            <Route path="/Reviews/:movieId" element ={<Reviews getMovieData = {getMovieData} movie={movie} reviews ={reviews} setReviews = {setReviews} />}></Route>
+          <Route path="*" element = {<NotFound/>}></Route>
+        </Route>
+      </Routes>
     </div>
   );
 }
